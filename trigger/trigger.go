4f161063e8a686dddc4ffca84f312b084ad4f958
pankona <yosuke.akatsuka@gmail.com>
2018-07-23 15:13:50 +0900

    change function names

--- trigger/trigger.go
+++ trigger/trigger.go
@@ -30,6 +30,11 @@ func init() {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
+	circleCIBuild(w, r)
+	dockerBuild(w, r)
+}
+
+func circleCIBuild(w http.ResponseWriter, r *http.Request) {
 	circleci_url := "https://circleci.com/api/v1/project/pankona/gomo-simra/tree/master"
 	query := url.Values{"circle-token": {getEnvVar("CIRCLECI_API_KEY")}}
 
@@ -51,4 +56,8 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "err != nil")
 		fmt.Fprint(w, err)
 	}
+}
+
+func dockerBuild(w http.ResponseWriter, r *http.Request) {
+	//curl -H "Content-Type: application/json" --data '{"docker_tag": "master"}' -X POST https://registry.hub.docker.com/u/pankona/gomo-simra/trigger/8e5d67eb-c9b3-4b1e-bde1-1cbaad06d47b/
 }
